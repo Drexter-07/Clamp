30d9a4944df3d962f876620e63f6c1d686508790
Drexter-07 <59890906+Drexter-07@users.noreply.github.com>
2023-04-06 15:16:53 +0530

    Initial Commit

---
+++ src/pages/Desktop1.js
@@ -0,0 +1,177 @@
+import "./Desktop1.css";
+
+const Desktop1 = () => {
+  return (
+    <div className="desktop-1">
+      <div className="desktop-1-child" />
+      <div className="desktop-1-item" />
+      <div className="desktop-1-inner" />
+      <div className="total-revenue-92983-container">
+        <p className="total-revenue">Total Revenue</p>
+        <p className="p">$92,983</p>
+      </div>
+      <div className="rectangle-div" />
+      <div className="join-our-online-container">
+        <p className="join-our-online">Join our online community. It helps</p>
+        <p className="total-revenue">in managing your projects.</p>
+      </div>
+      <b className="dear-client">Dear Client</b>
+      <div className="desktop-1-child1" />
+      <b className="travel">Travel</b>
+      <div className="desktop-1-child2" />
+      <div className="desktop-1-child3" />
+      <img className="file-filled-icon" alt="" src="/filefilled.svg" />
+      <div className="dashboard-email-calender-container">
+        <p className="dashboard">
+          <b className="dashboard1">Dashboard</b>
+          <span>{` `}</span>
+        </p>
+        <p className="dashboard">&nbsp;</p>
+        <p className="dashboard"> Email</p>
+        <p className="dashboard">&nbsp;</p>
+        <p className="dashboard">Calender</p>
+        <p className="dashboard">&nbsp;</p>
+        <p className="dashboard">Graph</p>
+        <p className="dashboard">&nbsp;</p>
+        <p className="total-revenue">Forms</p>
+      </div>
+      <div className="desktop-1-child4" />
+      <div className="desktop-1-child5" />
+      <img className="arrow-icon" alt="" src="/arrow-2.svg" />
+      <b className="free-slots-available">Free Slots Available</b>
+      <div className="desktop-1-child6" />
+      <div className="desktop-1-child7" />
+      <b className="b">75%</b>
+      <div className="projects-progress">Projects Progress</div>
+      <div className="desktop-1-child8" />
+      <div className="uiux-designer">UI/UX Designer</div>
+      <div className="desktop-1-child9" />
+      <b className="go-pro">Go Pro</b>
+      <div className="hira-r">Hira R.</div>
+      <div className="uxui-designer">UX/UI Designer</div>
+      <div className="projects">
+        <p className="p1">125</p>
+        <p className="total-revenue">Projects</p>
+      </div>
+      <div className="revenue">
+        <p className="p1">$124</p>
+        <p className="total-revenue">Revenue</p>
+      </div>
+      <img className="ellipse-icon" alt="" src="/ellipse-1.svg" />
+      <div className="div">$</div>
+      <div className="desktop-1-child10" />
+      <div className="desktop-1-child11" />
+      <b className="hotel">Hotel</b>
+      <div className="mobile-application-seen-container">
+        <p className="join-our-online">Mobile Application</p>
+        <p className="seen-by">Seen by</p>
+      </div>
+      <div className="dashboard-seen-by-container">
+        <p className="join-our-online">Dashboard</p>
+        <p className="seen-by">Seen by</p>
+      </div>
+      <b className="transaction-detail">Transaction Detail</b>
+      <div className="hi-hira">Hi Hira</div>
+      <b className="b1">$4,763.40</b>
+      <div className="available-balance">Available Balance</div>
+      <div className="div1">****3863</div>
+      <b className="upgrade-your-plan-container">
+        <p className="join-our-online">Upgrade your plan to</p>
+        <p className="total-revenue"> get pro benefits</p>
+      </b>
+      <img className="desktop-1-child12" alt="" src="/ellipse-2.svg" />
+      <img className="asset-14x-3" alt="" src="/asset-14x-3@2x.png" />
+      <img className="desktop-1-child13" alt="" src="/ellipse-2.svg" />
+      <img className="asset-14x-5" alt="" src="/asset-14x-3@2x.png" />
+      <img className="desktop-1-child14" alt="" src="/ellipse-2.svg" />
+      <img className="asset-14x-4" alt="" src="/asset-14x-3@2x.png" />
+      <img className="desktop-1-child15" alt="" src="/ellipse-5.svg" />
+      <img className="asset-14x-6" alt="" src="/asset-14x-6@2x.png" />
+      <img className="desktop-1-child16" alt="" src="/ellipse-5.svg" />
+      <img className="asset-14x-7" alt="" src="/asset-14x-6@2x.png" />
+      <img className="desktop-1-child17" alt="" src="/ellipse-5.svg" />
+      <img className="asset-14x-8" alt="" src="/asset-14x-6@2x.png" />
+      <img className="image-167-icon" alt="" src="/image-167@2x.png" />
+      <div className="desktop-1-child18" />
+      <div className="june">
+        <p className="p3">12</p>
+        <p className="june1">June</p>
+      </div>
+      <div className="desktop-1-child19" />
+      <div className="june2">
+        <p className="p3">18</p>
+        <p className="june1">June</p>
+      </div>
+      <div className="desktop-1-child20" />
+      <div className="june4">
+        <p className="p3">20</p>
+        <p className="june1">June</p>
+      </div>
+      <div className="desktop-1-child21" />
+      <div className="july">
+        <p className="p3">02</p>
+        <p className="june1">July</p>
+      </div>
+      <div className="desktop-1-child22" />
+      <div className="july2">
+        <p className="p3">15</p>
+        <p className="june1">July</p>
+      </div>
+      <div className="desktop-1-child23" />
+      <div className="july4">
+        <p className="p3">10</p>
+        <p className="june1">July</p>
+      </div>
+      <img className="image-123-icon" alt="" src="/image-123@2x.png" />
+      <img className="image-124-icon" alt="" src="/image-124@2x.png" />
+      <img className="image-126-icon" alt="" src="/image-126@2x.png" />
+      <img className="image-218-icon" alt="" src="/image-218@2x.png" />
+      <img className="desktop-1-child24" alt="" src="/ellipse-8.svg" />
+      <img className="desktop-1-child25" alt="" src="/arrow-1.svg" />
+      <div className="desktop-1-child26" />
+      <b className="send">Send</b>
+      <img className="calendar-empty-icon" alt="" src="/calendarempty.svg" />
+      <img className="vector-icon" alt="" src="/vector.svg" />
+      <img className="bar-chart-01-icon" alt="" src="/barchart01.svg" />
+      <div className="desktop-1-child27" />
+      <img className="double-folder-icon" alt="" src="/double-folder.svg" />
+      <img className="rectangle-icon" alt="" src="/rectangle-23.svg" />
+      <img className="desktop-1-child28" alt="" />
+      <b className="lets-start">Let’s Start</b>
+      <div className="project-details">Project Details</div>
+      <div className="hira-r1">Hira R.</div>
+      <div className="desktop-1-child29" />
+      <div className="desktop-1-child30" />
+      <div className="tour">Tour</div>
+      <div className="desktop-1-child31" />
+      <div className="trip">Trip</div>
+      <div className="desktop-1-child32" />
+      <div className="transport">Transport</div>
+      <div className="desktop-1-child33" />
+      <div className="tour1">Tour</div>
+      <div className="desktop-1-child34" />
+      <div className="trip1">Trip</div>
+      <div className="desktop-1-child35" />
+      <div className="transport1">Transport</div>
+      <img className="desktop-1-child36" alt="" src="/ellipse-2.svg" />
+      <img className="asset-14x-9" alt="" src="/asset-14x-3@2x.png" />
+      <img className="line-icon" alt="" src="/line-1.svg" />
+      <img className="desktop-1-child37" alt="" src="/line-2.svg" />
+      <div className="project-progress">Project Progress</div>
+      <div className="remaining-projects">3 Remaining Projects</div>
+      <img className="desktop-1-child38" alt="" src="/ellipse-11.svg" />
+      <img className="desktop-1-child39" alt="" src="/ellipse-10.svg" />
+      <div className="div2">75%</div>
+      <img className="desktop-1-child40" alt="" src="/ellipse-13.svg" />
+      <img className="menu-vertical-icon" alt="" src="/menu-vertical@2x.png" />
+      <img
+        className="menu-vertical-icon1"
+        alt=""
+        src="/menu-vertical1@2x.png"
+      />
+      <img className="droplets-1-icon" alt="" src="/droplets-1@2x.png" />
+    </div>
+  );
+};
+
+export default Desktop1;
